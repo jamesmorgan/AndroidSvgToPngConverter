daf37d30764df62ba42d54ac5c4d9a7260f6d224
James Morgan <james.morgan.e@googlemail.com>
2011-05-15 10:43:19 +0100

    Inital SVG to PNG conversion class

---
+++ src/com/morgan/design/SizeGroups.groovy
@@ -0,0 +1,50 @@
+package com.morgan.design
+
+
+/**
+ * @author James Edward Morgan
+ */
+class SizeGroups {
+
+
+	def static  ANDROID_MENU_SIZES = [
+		[_36_PX, _36_PX],
+		[_48_PX, _48_PX],
+		[_72_PX, _72_PX
+		]
+	];
+
+	def static ANDROID_LAUNCHER_SIZES = [
+		[_36_PX, _36_PX],
+		[_48_PX, _48_PX],
+		[_72_PX, _72_PX
+		]
+	];
+
+	def static  ANDROID_2_2_STATUS_BAR = [
+		[_19_PX, _19_PX],
+		[_25_PX, _25_PX],
+		[_38_PX, _38_PX]
+	];
+
+
+	def static  ANDROID_TAB = [
+		[_24_PX, _24_PX],
+		[_32_PX, _32_PX],
+		[_48_PX, _48_PX]
+	];
+
+	def static  ANDROID_DIALOG = ANDROID_TAB;
+	def static  ANDROID_LIST_VIEW = ANDROID_TAB;
+
+	def static FOLDERS =["ldpi", "mdpi", "hdpi"]
+
+	private static final int _19_PX = 19;
+	private static final int _24_PX = 24;
+	private static final int _25_PX = 25;
+	private static final int _36_PX = 36;
+	private static final int _32_PX = 32;
+	private static final int _38_PX = 38;
+	private static final int _48_PX = 48;
+	private static final int _72_PX = 72;
+}
---
+++ src/com/morgan/design/SvgImageCreator.groovy
@@ -0,0 +1,41 @@
+package com.morgan.design
+
+import org.apache.batik.apps.rasterizer.SVGConverter
+
+/**
+ * @author James Edward Morgan
+ */
+class SvgImageCreator {
+
+	public static void main(String[] args) {
+		new SvgImageCreator().convert(SizeGroups.ANDROID_DIALOG, [
+			"menu_bug",
+			"menu_clear_stats",
+			"menu_credits",
+			"menu_email",
+			"menu_improvement",
+			"menu_play_again",
+			"menu_settings",
+			"menu_manage_groups",
+			"menu_add_group"
+		])
+	}
+
+	def convert(def sizes, def filesToConvert){
+		filesToConvert.each{ file ->
+			sizes.eachWithIndex { i, index ->
+				println """Converting ${file} to W:${i[0]} H:${i[1]} for folder: ${SizeGroups.FOLDERS[index]}"""
+				convertToPng(file, SizeGroups.FOLDERS[index], i[0], i[1])
+			}
+		}
+	}
+
+	def convertToPng(def name, def folder, def width, def height){
+		final SVGConverter svgConverter = new SVGConverter();
+		svgConverter.setSources(["images/svg/${name}.svg"]as String[]);
+		svgConverter.setHeight(height);
+		svgConverter.setWidth(width);
+		svgConverter.setDst(new File("images/${folder}/${name}.png"));
+		svgConverter.execute();
+	}
+}
